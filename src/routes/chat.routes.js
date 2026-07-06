31e0b72dbc680919ee54a3982a05c0282d069f2d
Sami Abdullah <sami38596@gmailcom>
2026-07-06 07:11:21 +0600

    Fully remove chat feature files

--- src/routes/chat.routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { sendChatMessage } from '../controllers/chat.controller.js';
-
-const router = Router();
-
-router.post('/', sendChatMessage);
-
-export default router;
